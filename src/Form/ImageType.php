9c93b90fc1f4ed346abc75f7a15d1653bd0f7e16
Ionut <ionut_c1999@yahoo.com>
2024-12-02 21:42:09 +0200

    small changes

--- src/Form/ImageType.php
+++ src/Form/ImageType.php
@@ -23,7 +23,7 @@ class ImageType extends AbstractType
                 'required' => true,
                 'constraints' => [
                     new File([
-                        'maxSize' => '5M',
+                        'maxSize' => '15M',
                         'mimeTypes' => ['image/jpeg', 'image/png'],
                         'mimeTypesMessage' => 'Please upload a valid image file (JPEG or PNG).',
                     ])
