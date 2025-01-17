f422099421e3363c1fcc4dc2ce3d613b33c01468
Ionut <ionut_c1999@yahoo.com>
2025-01-17 22:44:53 +0200

    contact page implemented with backend

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,17 +2,39 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $message = new Contact();
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contact = $form->getData();
+            $message->setName($contact->getName());
+            $message->setEmail($contact->getEmail());
+            $message->setSubject($contact->getSubject());
+            $message->setMessage($contact->getMessage());
+
+            $entityManager->persist($contact);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Message sent successfully!');
+            return $this->redirectToRoute('app_contact');
+        }
+
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/ContactType.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Name',
+                'attr' => [
+                    'class' => "w-full rounded-md py-2.5 px-4 border text-sm outline-[#007bff]",
+                    'placeholder' => "Name",
+                    'required' => true
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => [
+                    'class' => "w-full rounded-md py-2.5 px-4 border text-sm outline-[#007bff]",
+                    'placeholder' => 'example@example.com',
+                    'required' => true
+                ]
+            ])
+            ->add('subject', TextType::class, [
+                'label' => 'Subject',
+                'attr' => [
+                    'class' => "w-full rounded-md py-2.5 px-4 border text-sm outline-[#007bff]",
+                    'placeholder' => 'Subject',
+                    'required' => true
+                ]
+            ])
+            ->add('message', TextareaType::class, [
+                'label' => 'Message',
+                'attr' => [
+                    'class' => "w-full rounded-md py-2.5 px-4 border text-sm outline-[#007bff]",
+                    'placeholder' => 'Your message...',
+                    'required' => true,
+                    'rows' => 5,
+                ]
+            ])
+            ->add('send', SubmitType::class, [
+                'label' => 'Send',
+                'attr' => [
+                    'class' => "text-white bg-[#007bff] hover:bg-blue-600 font-semibold rounded-md text-sm px-4 py-2.5 w-full"
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+        ]);
+    }
+}
