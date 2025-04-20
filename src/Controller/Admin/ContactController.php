5706e97176f0a387b53b863afc12652c43403535
Ionut Valentin <ionut_c1999@yahoo.com>
2025-04-21 00:22:36 +0300

    implemented knp paginator in admin messages

--- src/Controller/Admin/ContactController.php
+++ src/Controller/Admin/ContactController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Contact;
 use App\Service\ContactService;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,23 +15,42 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 #[IsGranted("ROLE_ADMIN")]
 class ContactController extends AbstractController
 {
+    public function __construct(
+        private readonly PaginatorInterface $paginator,
+    )
+    {
+    }
     #[Route('messages/unread', name: 'app_admin_messages_unread')]
-    public function unreadMessages(EntityManagerInterface $entityManager): Response
+    public function unreadMessages(EntityManagerInterface $entityManager, Request $request): Response
     {
         $messages = $entityManager->getRepository(Contact::class)->getUnreadDesc();
 
+        $pagination = $this->paginator->paginate(
+            $messages,
+            $request->query->getInt('page', 1),
+            8,
+        );
+
         return $this->render('admin/messages/unread.html.twig', [
             'messages' => $messages,
+            'pagination' => $pagination,
         ]);
     }
 
     #[Route('messages/read', name: 'app_admin_messages_read')]
-    public function readMessages(EntityManagerInterface $entityManager): Response
+    public function readMessages(EntityManagerInterface $entityManager, Request $request): Response
     {
         $messages = $entityManager->getRepository(Contact::class)->getReadDesc();
 
+        $pagination = $this->paginator->paginate(
+            $messages,
+            $request->query->getInt('page', 1),
+            8,
+        );
+
         return $this->render('admin/messages/read.html.twig', [
             'messages' => $messages,
+            'pagination' => $pagination,
         ]);
     }
 
